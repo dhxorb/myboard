4efd0817a11f950ff675e4a5b43833d95fef8f9a
dhxorb <128438308+dhxorb@users.noreply.github.com>
2024-12-11 14:53:28 +0000

    dsaf

---
+++ myboard/src/main/java/myboard/domain/Item.java
@@ -0,0 +1,12 @@
+package myboard.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item {
+    private Long id;
+    private String name;
+    private Long count;
+}
---
+++ myboard/src/main/java/myboard/dto/ItemDto.java
@@ -0,0 +1,10 @@
+package myboard.dto;
+
+import lombok.Data;
+
+@Data
+public class ItemDto {
+    private Long id;
+    private String name;
+    private Long count;
+}
